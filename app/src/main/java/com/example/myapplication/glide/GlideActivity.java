de08e8edaa12d222b3da3abcba135499a25e9a08
niyangup <niyangup@163.com>
2019-07-25 10:50:11 +0800

    新增Glide加载图片基本用法

---
+++ app/src/main/java/com/example/myapplication/glide/GlideActivity.java
@@ -0,0 +1,13 @@
+package com.example.myapplication.glide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GlideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glide);
+    }
+}
